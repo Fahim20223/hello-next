6eae855e47d879fd57f0931d5ba34600421d1737
Fahim2023 <fahimshahrier2023@gmail.com>
2025-12-24 12:33:13 +0600

    added [slug]

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import dashboard from "./page";
+import Link from "next/link";
 
 const DashboardLayout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="grid grid-cols-12 gap-5 min-h-screen">
+      <div className="col-span-3 border-r-2">
+        <h2>Navigation</h2>
+        <div className="flex flex-col gap-5 mt-10">
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded text-white"
+            href={"/dashboard/add-story"}
+          >
+            Add Story
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded text-white"
+            href={"/dashboard/my-profile"}
+          >
+            Add Profile
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded text-white"
+            href={"/dashboard/settings"}
+          >
+            Settings
+          </Link>
+        </div>
+      </div>
+      <div className="col-span-9">{children}</div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
